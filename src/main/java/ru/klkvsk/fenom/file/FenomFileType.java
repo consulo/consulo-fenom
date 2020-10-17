1268a07801e0fb1313956faa95162ceea9bac2d2
VISTALL <vistall.valeriy@gmail.com>
2020-10-17 14:37:10 +0300

    migrated to icon groups

--- src/main/java/ru/klkvsk/fenom/file/FenomFileType.java
+++ src/main/java/ru/klkvsk/fenom/file/FenomFileType.java
@@ -3,17 +3,11 @@ package ru.klkvsk.fenom.file;
 import javax.annotation.Nonnull;
 
 import javax.annotation.Nullable;
-import com.intellij.openapi.editor.colors.EditorColorsScheme;
-import com.intellij.openapi.editor.highlighter.EditorHighlighter;
-import com.intellij.openapi.fileTypes.EditorHighlighterProvider;
-import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.FileTypeEditorHighlighterProviders;
+
 import com.intellij.openapi.fileTypes.LanguageFileType;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
+import consulo.fenom.icon.FenomIconGroup;
 import consulo.ui.image.Image;
 import ru.klkvsk.fenom.FenomLanguage;
-import ru.klkvsk.fenom.editor.FenomTemplateHighlighter;
 
 public class FenomFileType extends LanguageFileType
 {
@@ -49,6 +43,6 @@ public class FenomFileType extends LanguageFileType
 	@Override
 	public Image getIcon()
 	{
-		return FenomIcons.FILE;
+		return FenomIconGroup.filetype_icon();
 	}
 }
--- src/main/java/ru/klkvsk/fenom/file/FenomIcons.java
+++
@@ -1,9 +0,0 @@
-package ru.klkvsk.fenom.file;
-
-import com.intellij.openapi.util.IconLoader;
-import consulo.ui.image.Image;
-
-public class FenomIcons
-{
-	public static final Image FILE = IconLoader.getIcon("/ru/klkvsk/fenom/file/filetype-icon.png");
-}
